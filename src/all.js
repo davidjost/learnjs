a4a6ea687dce573d055741214ca492facc1e7aad
Gregmcor <gregmcor@users.noreply.github.com>
2018-01-22 21:21:38 +0100

    finished lesson 03

--- src/all.js
+++ src/all.js
@@ -1,3 +1,5 @@
+// browser-sync start --server 'src' --files 'src'
+
 // string
 console.log('this is a string')
 
@@ -5,7 +7,24 @@ console.log('this is a string')
 console.log('string 1' + ' string 2')
 
 // number
-console.log(23434)
+console.log('number: ' + 23434)
 
 // adding numbers
-console.log(2324234 + 34234)
+const adding = 2 + 3
+console.log('adding: ' + adding)
+
+// subtracting numbers
+const subtracting = 12 - 3
+console.log('subtracting: ' + subtracting)
+
+// multiplying numbers
+const multiplying = 4 * 2
+console.log('multiplying: ' + multiplying)
+
+// division
+const division = 4 / 2
+console.log('division: ' + division)
+
+// booleans
+const bool = true
+console.log(bool)
