4b5125f9383a6544494f2b185b5818c82aad37de
David Jost <david.jost@rewe-digital.com>
2018-01-22 11:40:11 +0100

    initial commit

---
+++ all.js
@@ -0,0 +1,11 @@
+// string
+console.log('this is a string')
+
+// adding 2 strings together
+console.log('string 1' + ' string 2')
+
+// number
+console.log(23434)
+
+// adding numbers
+console.log(2324234 + 34234)
