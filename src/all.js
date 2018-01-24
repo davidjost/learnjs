0e9b7e8f1bdbeb00df4d4cf43ae69ebd4ae95a41
Gregmcor <gregmcor@users.noreply.github.com>
2018-01-24 22:13:53 +0100

    finished lesson 07 about functions

--- src/all.js
+++ src/all.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-// browser-sync start --server --files "*" --startPath "/src"
-=======
 // browser-sync start --server 'src' --files 'src'
->>>>>>> a4a6ea687dce573d055741214ca492facc1e7aad
+// ------------------------------------ lesson 01 - basics
 
 // string
 console.log('this is a string')
@@ -14,12 +11,10 @@ console.log('string 1' + ' string 2')
 console.log('number: ' + 23434)
 
 // adding numbers
-<<<<<<< HEAD
 console.log(2324234 + 34234)
 
 // subtracting numbers
 console.log(34324 - 31234)
-=======
 const adding = 2 + 3
 console.log('adding: ' + adding)
 
@@ -38,4 +33,43 @@ console.log('division: ' + division)
 // booleans
 const bool = true
 console.log(bool)
->>>>>>> a4a6ea687dce573d055741214ca492facc1e7aad
+
+// ------------------------------------ lesson 07 - functions
+
+function sayName (firstName, lastName) {
+  console.log('First name: ' + firstName)
+  console.log('Last name: ' + lastName)
+}
+sayName('david', 'jost')
+
+function get2 () {
+  return 'sefsdfsdf'
+  console.log('blah')
+}
+const results = get2()
+console.log(results) // so bekommt man ergebnisse aus dem function scope raus
+
+function add2 (num) {
+  return num + 2
+}
+const number = add2(8)
+console.log(number)
+
+// excercises
+
+function logger (toLog) {
+  console.log(toLog)
+}
+logger('tree')
+
+function add (num1, num2) {
+  return num1 + num2
+}
+const addResult = add(2, 2)
+console.log(addResult)
+
+function multy (num1, num2) {
+  return num1 * num2
+}
+const multyResult = multy(2, 4)
+console.log(multyResult)
